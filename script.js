68318cb4817a3963d692d4444df54208142138cf
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-20 18:27:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Math = built-in object the provides a
+//        collection of properties and methods
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x = 3;
+let y = 2;
+// let z;
+let z = 1;
+
+// z = Math.round(x);
+// z = Math.floor(x);
+// z = Math.ceil(x);
+// z = Math.trunc(x);
+// z = Math.pow(y, x);
+// z = Math.sqrt(x);
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+
+let max = Math.max(x, y, z)
+let min = Math.min(x, y, z)
+
+console.log(min);
